62b1e7b848b0b647559c8cfe5601d6bb2feb7d45
Subodh Tavargeri <subodh.tavargeri@gmail.com>
2022-10-06 20:21:29 +0530

    updated router spy

--- Tdd2ChessGame2Tests/GameTimer/Spy/GameTimerRouterSpy.swift
+++ Tdd2ChessGame2Tests/GameTimer/Spy/GameTimerRouterSpy.swift
@@ -8,17 +8,13 @@
 import Foundation
 @testable import Tdd2ChessGame2
 
-class GameTimerRouterSpy: GameTimerRouterProtocol {
-
-    var invokedRouteToPlayerTimer = false
-    var invokedRouteToPlayerTimerCount = 0
-    var invokedRouteToPlayerTimerParameters: (controller: GameTimerViewController, Void)?
-    var invokedRouteToPlayerTimerParametersList = [(controller: GameTimerViewController, Void)]()
-
-    func routeToPlayerTimer(controller: GameTimerViewController) {
-        invokedRouteToPlayerTimer = true
-        invokedRouteToPlayerTimerCount += 1
-        invokedRouteToPlayerTimerParameters = (controller, ())
-        invokedRouteToPlayerTimerParametersList.append((controller, ()))
+class GameTimerRouterSpy: RouterProtocol {
+    func routeToGameTimer() {
+        
     }
+    
+    func routeToPlayerTimer() {
+        
+    }
+
 }
